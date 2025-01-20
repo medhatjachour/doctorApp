b7f218bf691a9b1093d878504bea85963fe5fae3
medhatjachour <53466991+medhatjachour@users.noreply.github.com>
2025-01-20 16:26:56 +0200

    init
    add routing and navbar

---
+++ frontend/src/App.tsx
@@ -0,0 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
+import MyProfile from "./pages/MyProfile";
+import MyAppointments from "./pages/MyAppointments";
+import Appointments from "./pages/Appointments";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="mx-4 sm:mx-[10%] ">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:specialty" element={<Doctors />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/appointment/:docId" element={<Appointments />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
